a5fa8f995f9e89ae0982d0e1bdd41529f9afc7ff
Stanko Cerin <stanko.cerin@ostendogroup.com>
2023-11-23 12:41:32 +0100

    removes debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,6 @@ class ArticlesController < ApplicationController
   def show
      # @ sign turns variable into instanca variable which is available outside the action
      # params are parameters taken from the URL (e.g. /articles/1 - 1 in this case)
-     debugger
 
 
     @article = Article.find(params[:id])
