8dd0cc33074a417eef46f5a3166a48b759dac2c8
Stanko Cerin <stanko.cerin@ostendogroup.com>
2023-11-25 07:19:45 +0100

    Add articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,10 @@ class ArticlesController < ApplicationController
   def show
      # @ sign turns variable into instanca variable which is available outside the action
      # params are parameters taken from the URL (e.g. /articles/1 - 1 in this case)
-
-
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles,  only: [:show]
+  # to see a list of all routes resources command created,
+  # go to terminal and write "rails routes --expanded".
+  # Than you can use only: command to crerat only the needed ones.
+  resources :articles,  only: [:show, :index]
 end
